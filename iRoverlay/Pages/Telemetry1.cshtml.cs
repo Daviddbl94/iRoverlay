9caf76304acc425592bbbc8a6e8b5343814e08e9
davbla1994 <dblancowork@gmail.com>
2024-02-11 02:46:39 +0100

    A

---
+++ iRoverlay/Pages/Telemetry1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace iRoverlay.Pages
+{
+    public class Telemetry1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ iRoverlay/TelemetryHub.cs
@@ -0,0 +1,12 @@
+﻿namespace iRoverlay
+{
+    public class TelemetryHub
+    {
+        public async Task SendTelemetryData(TelemetryData telemetryData)
+        {
+            // Aquí procesa los datos de telemetría recibidos
+            // y envía la información actualizada a todos los clientes
+            await Clients.All.SendAsync("ReceiveTelemetryData", telemetryData);
+        }
+    }
+}
