e4d1e7d77179c7cf941050d0edbaab2375020487
davbla1994 <dblancowork@gmail.com>
2024-02-13 15:59:31 +0100

    Añadidos cambios signalIr.

--- iRoverlay/MyHub .cs
+++ iRoverlay/MyHub .cs
@@ -1,13 +1,10 @@
-﻿using Microsoft.AspNet.SignalR;
-using Microsoft.AspNetCore.SignalR;
+﻿// MyHub.cs
+using Microsoft.AspNet.SignalR;
 
 namespace iRoverlay
 {
     public class MyHub : Hub
     {
-        public void SendMessage(string message)
-        {
-            Clients.All.ReceiveMessage(message); // Define el método para enviar mensajes a los clientes
-        }
+        //Clients.All.ReceiveMessage(message);
     }
 }
